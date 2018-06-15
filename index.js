2c2a16d4d88a98e27879c56fed32d2bb82931a6f
cerylean@gmail.com <cerylean@gmail.com>
2018-06-15 13:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function divide(a, b) {return a / b}
 function inc(n) {return n += 1}
 function dec(n) {return n -= 1}
 function makeInt(n) {return parseInt(n,10)}
-function preserveDecimal (n) {return parseFloat(2.222)}
+function preserveDecimal (n) {return parseFloat(n)}
 function preserveDecimal (n) {return parseFloat('nonsense')}
